ea63fd028ebef80415444180c1a5ecb3241cdfb1
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-10-30 17:23:10 -0700

    added get request for api/users

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const db = require("./data/db.js");
+
+const express = require("express");
+
+const server = express();
+
+server.listen(4000, () => {
+  console.log("=== server listening on port 4000 ===");
+});
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+});
